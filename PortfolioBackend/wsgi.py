550f2653cdc453738fb32ae1ca59bc6c49aa256d
MrTuzzO <khirulislam5750@gmail.com>
2025-08-13 02:54:23 +0600

    vercel setup added

--- PortfolioBackend/wsgi.py
+++ PortfolioBackend/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PortfolioBackend.settings')
 
 application = get_wsgi_application()
+
+# Vercel requires `app` or `handler` at module level
+app = application
